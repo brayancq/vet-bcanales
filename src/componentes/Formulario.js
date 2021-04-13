c536845dd59b9d61111b97282850211972f632ad
brayancq <brayanc.q@gmail.com>
2021-04-13 14:42:33 -0600

    cambios kamill

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -5,60 +5,61 @@ class Formulario extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <h1>Datos de la mascota</h1>
-
-        <form>
-          <div className="form-group">
-            <label>Nombre</label>
-            <input
-              name="nombre"
-              onChange={this.props.onChange}
-              type="text"
-              className="form-control-inputs"
-              id="nombre"
-              value={this.props.formValues.nombre}
-            />
-            <label>Raza</label>
-            <input
-              name="raza"
-              onChange={this.props.onChange}
-              type="text"
-              className="form-control-inputs"
-              id="raza"
-              value={this.props.formValues.raza}
-            />
-            <label>Nombre del dueño</label>
-            <input
-              name="nombreDue"
-              onChange={this.props.onChange}
-              type="text"
-              className="form-control-inputs"
-              id="nombreDue"
-              value={this.props.formValues.nombreDue}
-            />
-            <label>Teléfono</label>
-            <input
-              name="telefono"
-              onChange={this.props.onChange}
-              type="text"
-              className="form-control-inputs"
-              id="telefono"
-              value={this.props.formValues.telefono}
-            />
-            <label>Correo</label>
-            <input
-              name="correo"
-              onChange={this.props.onChange}
-              type="email"
-              className="form-control-inputs"
-              id="correo"
-              value={this.props.formValues.correo}
-            />
-          </div>
-          <Link to="/listado" className="btn btn-primary">
-            Guardar
-          </Link>
-        </form>
+        <div className="contForm">
+          <h1>Datos de la mascota</h1>
+          <form>
+            <div className="form-group">
+              <label>Nombre</label>
+              <input
+                name="nombre"
+                onChange={this.props.onChange}
+                type="text"
+                className="form-control-inputs"
+                id="nombre"
+                value={this.props.formValues.nombre}
+              />
+              <label>Raza</label>
+              <input
+                name="raza"
+                onChange={this.props.onChange}
+                type="text"
+                className="form-control-inputs"
+                id="raza"
+                value={this.props.formValues.raza}
+              />
+              <label>Nombre del dueño</label>
+              <input
+                name="nombreDue"
+                onChange={this.props.onChange}
+                type="text"
+                className="form-control-inputs"
+                id="nombreDue"
+                value={this.props.formValues.nombreDue}
+              />
+              <label>Teléfono</label>
+              <input
+                name="telefono"
+                onChange={this.props.onChange}
+                type="text"
+                className="form-control-inputs"
+                id="telefono"
+                value={this.props.formValues.telefono}
+              />
+              <label>Correo</label>
+              <input
+                name="correo"
+                onChange={this.props.onChange}
+                type="email"
+                className="form-control-inputs"
+                id="correo"
+                value={this.props.formValues.correo}
+              />
+            </div>
+            <Link to="/listado" className="btn btn-primary">
+              Guardar
+            </Link>
+          </form>
+        </div>
       </React.Fragment>
     );
   }
